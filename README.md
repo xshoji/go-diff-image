acde12bc7243da81ba4a0775ba81e32259c6eb85
xshoji <xshoji@users.noreply.github.com>
2023-04-30 16:10:42 +0900

    fix

--- README.md
+++ README.md
@@ -10,8 +10,18 @@ $ go get -u github.com/xshoji/go-diff-image/...
 
 ## Usage
 ```bash
-diff-image -o=diff.png before.png after.png
-# open diff.png
+go run cmd/diff-image/main.go
+Usage of /var/folders/_q/dpw924t12bj25568xfxcd2wm0000gn/T/go-build683702176/b001/exe/main:
+    -a, --after-image-path string
+    	[required] Image path (after)
+    -b, --before-image-path string
+    	[required] Image path (before)
+    -h, --help
+    	Show help
+    -o, --output string
+    	[required] Output path
+
+go run cmd/diff-image/main.go -o=/tmp/d.png -b=/tmp/s1.png -a=/tmp/s2.png
 ```
 ## Example
 
--- cmd/diff-image/main.go
+++ cmd/diff-image/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	diffimage "github.com/xshoji/go-diff-image"
 	"image"
 	"image/png"
 	"log"
 	"os"
-
-	diffimage "github.com/xshoji/go-diff-image"
+	"regexp"
+	"runtime"
 )
 
 func mustOpen(filename string) *os.File {
@@ -47,23 +49,59 @@ func rate16(c uint32, r float64) uint16 {
 	return uint16(65535 - (65535-fc)*r)
 }
 
-func main() {
-	var (
-		output string
-	)
-	flag.StringVar(&output, "output", "diff.png", "output filename")
-	flag.StringVar(&output, "o", "diff.png", "output filename")
-	flag.Parse()
-	args := flag.Args()
-	if len(args) != 2 {
-		fmt.Println("usage: imagediff [<option>...] <image1> <image2>")
-		os.Exit(1)
+const (
+	DummyUsage = "########"
+)
+
+var (
+	TerminalColorReset  = "\033[0m"
+	TerminalColorYellow = "\033[33m"
+	// Define short parameters ( don't set default value ).
+	paramsOutputPath      = flag.String("o", "", DummyUsage)
+	paramsBeforeImagePath = flag.String("b", "", DummyUsage)
+	paramsAfterImagePath  = flag.String("a", "", DummyUsage)
+	paramsHelp            = flag.Bool("h", false, "\n"+DummyUsage)
+)
+
+func init() {
+
+	if runtime.GOOS == "windows" {
+		TerminalColorReset = ""
+		TerminalColorYellow = ""
 	}
 
-	img1 := mustLoadImage(args[0])
-	img2 := mustLoadImage(args[1])
+	// Define long parameters and description ( set default value here if you need ).
+	// Required parameters
+	flag.StringVar(paramsOutputPath /*      */, "output" /*            */, "" /*     */, TerminalColorYellow+"[required]"+TerminalColorReset+" Output path")
+	flag.StringVar(paramsBeforeImagePath /* */, "before-image-path" /* */, "" /*     */, TerminalColorYellow+"[required]"+TerminalColorReset+" Image path (before)")
+	flag.StringVar(paramsAfterImagePath /*  */, "after-image-path" /*   */, "" /*     */, TerminalColorYellow+"[required]"+TerminalColorReset+" Image path (after)")
+	// Optional parameters
+	flag.BoolVar(paramsHelp /*              */, "help" /*              */, false /*  */, "Show help")
+}
+
+func main() {
+
+	// Set DummyUsage
+	b := new(bytes.Buffer)
+	flag.CommandLine.SetOutput(b)
+	flag.Usage()
+	re := regexp.MustCompile("(-\\S+)( *\\S*)+\n*\\s+" + DummyUsage + "\n*\\s+(-\\S+)( *\\S*)+\n")
+	usage := re.ReplaceAllString(b.String(), "  $1, -$3$4\n")
+	flag.CommandLine.SetOutput(os.Stderr)
+	flag.Usage = func() {
+		_, _ = fmt.Fprintf(flag.CommandLine.Output(), usage)
+	}
+
+	flag.Parse()
+	if *paramsHelp || *paramsOutputPath == "" || *paramsBeforeImagePath == "" || *paramsAfterImagePath == "" {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	img1 := mustLoadImage(*paramsBeforeImagePath)
+	img2 := mustLoadImage(*paramsAfterImagePath)
 
 	dst := diffimage.DiffImage(img1, img2)
 
-	mustSaveImage(dst, output)
+	mustSaveImage(dst, *paramsOutputPath)
 }
